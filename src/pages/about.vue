573da1af986bb7ef71ba371649d85943988e75f2
vikash2806 <vikashrajendran2806@gmail.com>
2021-07-24 09:21:11 +0530

    tweet form added

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,6 +1,8 @@
 <template>
-  <q-page class="">
-  <h5>this is about page</h5>
+  <q-page class="q-pa-lg">
+  <h5 class="q-mt-none">this is about page</h5>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias, aspernatur quaerat. Repellat, quaerat! Magnam vel velit explicabo maiores neque ipsa.</p>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur odio necessitatibus nemo fuga alias itaque provident, ab iure, quibusdam quae architecto. Quas repudiandae voluptatibus quam. Blanditiis fugit esse debitis mollitia?</p>
   </q-page>
 </template>
 
--- src/pages/pageHome.vue
+++ src/pages/pageHome.vue
@@ -1,13 +1,47 @@
 <template>
-  <q-page class="">
-    <h4>Home</h4>
+  <q-page>
+    <div class="q-py-lg q-px-lg row items-end q-col-gutter-lg">
+      <div class="col">
+        <q-input class="newTweet" autogrow bottom-slots v-model="newTweet" placeholder="What's up for today ?!" counter
+          maxlength="250">
+          <template v-slot:before>
+            <q-avatar size='xl'>
+              <img src="https://data.whicdn.com/images/344552644/original.jpg">
+            </q-avatar>
+          </template>
+        </q-input>
+      </div>
+      <div class="col col-shrink">
+      <q-btn class='q-mb-lg' :disable='!newTweet' push unelevated rounded color="primary" no-caps label="Tweet" />
+      </div>
+    </div>
+    <q-separator class='divider' size='10px' color="grey-2"/>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+  import {
+    defineComponent
+  } from 'vue';
 
-export default defineComponent({
-  name: 'PageHome'
-})
+  export default defineComponent({
+    name: 'PageHome',
+    data() {
+      return {
+        newTweet: '',
+      }
+    }
+  })
+
 </script>
+
+<style scoped lang='scss'>
+  .newTweet {
+    font-size: 19px;
+  }
+.divider{
+  border-top: 1px solid;
+  border-bottom: 1px solid ;
+  border-color: $grey-4;
+}
+</style>
